0395c638c1e76f4bfb8a445396f3df5db851b2e6
pijiuya <pjyoffice1999@gmail.com>
2023-12-06 05:27:29 +0800

    Sync new files from OSS

--- jsoncreat.py
+++ jsoncreat.py
@@ -1,50 +1,21 @@
 # -*- coding: utf-8 -*-
-import oss2
 import os
-import subprocess
+import json
+import time
 
-# Aliyun configuration
-ACCESS_KEY_ID = 'LTAI5t8xYRR3E9hgx2STRDdw'
-ACCESS_KEY_SECRET = 'XQ0Bp3VMjTVbVQojcvNmUrvHg4P7cR'
-ENDPOINT = 'oss-cn-beijing.aliyuncs.com'
-BUCKET_NAME = 'pjybucketname'
-OSS_FOLDER = 'GQrealtime/'
+def generate_image_list_json(directory, output_file):
+    """creat"""
+    images = [f for f in os.listdir(directory) if f.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    with open(output_file, 'w') as f:
+        json.dump(images, f)
 
-# Initialize OSS
-auth = oss2.Auth(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
-bucket = oss2.Bucket(auth, ENDPOINT, BUCKET_NAME)
+def main():
+    folder_to_watch = 'C:\\Users\\Administrator\\Desktop\\web\\download_photo' # 更改为您的图片文件夹路径
+    output_json = 'C:\\Users\\Administrator\\Desktop\\web\\image_list.json' # 输出JSON文件的路径
 
-# Local configuration
-LOCAL_FOLDER = 'C:\\Users\\Administrator\\Desktop\\web\\download_photo'
-REPO_PATH = 'C:\\Users\\Administrator\\Desktop\\web'
+    while True:
+        generate_image_list_json(folder_to_watch, output_json)
+        time.sleep(60) # 每60秒运行一次
 
-# Fetching OSS folder contents
-# Fetching OSS folder contents
-print("Fetching OSS folder contents...")
-oss_files = {obj.key[len(OSS_FOLDER):] for obj in oss2.ObjectIterator(bucket, prefix=OSS_FOLDER) if obj.key[-1] != '/'}
-
-# Fetching local folder contents
-print("Fetching local folder contents...")
-local_files = {f for f in os.listdir(LOCAL_FOLDER) if os.path.isfile(os.path.join(LOCAL_FOLDER, f))}
-
-# Download new files
-for file in oss_files:
-    if file not in local_files:
-        local_file_path = os.path.join(LOCAL_FOLDER, file)
-
-        # Ensure local file directory exists
-        local_file_dir = os.path.dirname(local_file_path)
-        if not os.path.exists(local_file_dir):
-            os.makedirs(local_file_dir)
-
-        # Download file
-        print(f"Downloading {file} to {local_file_path}")
-        bucket.get_object_to_file(OSS_FOLDER + file, local_file_path)
-
-# Git operations
-print("Running Git operations...")
-
-subprocess.run(['git', '-C', REPO_PATH, 'pull', 'origin', 'main'])
-subprocess.run(['git', '-C', REPO_PATH, 'add', '.'])
-subprocess.run(['git', '-C', REPO_PATH, 'commit', '-m', 'Sync new files from OSS'])
-subprocess.run(['git', '-C', REPO_PATH, 'push'])
+if __name__ == "__main__":
+    main()
